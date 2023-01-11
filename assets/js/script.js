1863330fa5e87846e2c03f03c0e3e378df8806d1
adam hovda <adamhovda@gmail.com>
2023-01-11 13:20:36 -0700

    added get element to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+let $cityinput = document.getElementById("city-input");
+let $dateinput = document.getElementById("date-input");
+let $hotels = document.getElementById("hotels").checked;
+let $rests = document.getElementById("rests").checked;
+let $attractions = document.getElementById("attractions").checked;
