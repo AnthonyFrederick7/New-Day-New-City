490412453822712bc8d911b89682ebcd718a5047
AnthonyFrederick7 <119832448+AnthonyFrederick7@users.noreply.github.com>
2023-01-12 15:56:11 -0600

    updated HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,18 +31,16 @@ $cityinput.addEventListener("focus", () => {
 });
 
 // Once submit is clicked, takes you to results page
-$submit.addEventListener("click", redirectFunction);
-function redirectFunction(event) {
+//$submit.addEventListener("click", redirectFunction);
+//function redirectFunction(event) {
     event.preventDefault()
 //Capture form input here
-window.location.replace($results)}
-//checkbox verification
-
+//window.location.replace($results)}
 function checkboxValidation(theForm) {
     if (
     theForm.hotels.checked == false &&
     theForm.restaurants.checked == false &&
-    theForm.attrations.checked == false)
+    theForm.attractions.checked == false)
     {
 
         $options.firstElementChild.style.color = "red";
@@ -52,6 +50,8 @@ function checkboxValidation(theForm) {
         return true;
     }
 }
+//checkbox verification
+
 
 // checks todays date and sets min for date picker
 let today = new Date().toISOString().split('T')[0];
